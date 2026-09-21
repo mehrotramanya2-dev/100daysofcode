e1b529fe6a6d95c0e96e5c7f43a0c38fd8e820b6
mehrotramanya2-dev <mehrotramanya2@gmail.com>
2026-09-21 14:10:16 +0530

    Add files via upload

---
+++ day_35_q69.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n, arr[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int first = INT_MIN, second = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == INT_MIN) printf("No second largest element\n");
+    else printf("%d\n", second);
+    return 0;
+}
---
+++ day_35_q70.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n, arr[100], k;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+
+    k = k % n;
+    /* reversal algorithm: reverse all, then each part */
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
